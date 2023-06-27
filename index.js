127e9be3d753b613e0d602c133f2b342866ed4e5
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-06-27 17:50:25 -0500

    working with var

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+var bestCustomer
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+const leastfavoriteCustomer = 'idk'
+function changeLeastFavoriteCustomer(){
+    return leastfavoriteCustomer = 'bob'
+}
